3930673fc8ae6f2106ac0035314490a56399ce58
MauricioNC <mauricionc19@gmail.com>
2024-03-14 13:19:17 -0600

    feat: Created form to login

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
-  def index
+  def new
+    @room = Room.new
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
   root 'home#home'
+  resources :rooms
 end
