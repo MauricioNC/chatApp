b12b226c491bf51b1d290343c5d54fe7ed61c87c
MauricioNC <mauricionc19@gmail.com>
2024-03-14 17:47:17 -0600

    feat: Added logic and configuration to send an email confirmation to the user

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,5 +4,15 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:room][:email])
+    if @user.nil?
+      render :new
+    else
+      UserMailer.with(user: @user).email_confirmation.deliver_later
+      respond_to do |format|
+        format.html {}
+        format.turbo_stream
+      end
+    end
   end
 end
