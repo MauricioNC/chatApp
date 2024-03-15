5aad82857b17832140c4d1872736cb2825d15132
MauricioNC <mauricionc19@gmail.com>
2024-03-15 13:13:24 -0600

    feat: Register token when the email confirmation is sent to the user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   def email_confirmation
     @user = params[:user]
     @token = TokenizeService.new({ email: @user.email, username: @user.username }).generate_token
+    Token.create(token: @token, tokenable: @user)
     mail(to: @user.email, subject: "Email confirmation" )
   end
 end
